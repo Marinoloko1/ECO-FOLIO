adb6822069d8c34dc6f48a385da8c6a173373c54
unknown <maryfernavarroherrera@gmail.com>
2024-05-15 17:31:40 -0600

    correcciones a la tabla de usuarios

--- app/Http/Controllers/PaginaController.php
+++ app/Http/Controllers/PaginaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class create_Usuarios_table extends Controller
+class PaginaController extends Controller
 {
     //
 }
--- database/migrations/2024_05_15_020255_create_usuarios_table.php
+++ database/migrations/2024_05_15_020255_create_usuarios_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("tajeta");
+            $table->string("tarjeta");
+            $table->string("colonia");
         });
     }
 
