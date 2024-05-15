63261d451946a43e5884eea05769e909d7758601
unknown <maryfernavarroherrera@gmail.com>
2024-05-15 16:41:08 -0600

    vista de usuarios

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> "Usuarios"</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script type="module" src="js/bootstrap.js"></script>
+    </head>
+
+    <body>
+        <header></header>
+
+        <main>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1> ID: </h1>
+                    </div>
+
+                    <div class="col">
+                        <h1> Nombre del usuario: </h1>
+                    </div>
+
+                    <div class="col">
+                        <h1>Uso del dispensador: </h1>
+                    </div>
+
+                    <div class="col">
+                        <h1>N. de tarjeta: </h1>
+                    </div>
+                </div>
+
+            <div class="row">
+                <div class="col">
+
+                </div>
+
+                <div class="col">
+
+                </div>
+
+                <div class="col">
+
+                </div>
+
+                <div class="col">
+
+                </div>
+            </div>    
+        </main>
+    </body>
+</html>
