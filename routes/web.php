244904fcda78424de9fb9a7b6b20e24baa5a904a
unknown <maryfernavarroherrera@gmail.com>
2024-05-17 22:56:59 -0600

    boton correcion

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Route::get("usuarios/{id}", [PaginaController::class, "verUsuario"]);
 
-Route::get("usuarios/{id}", [PaginaController::class, "verUsuarios"]);
+Route::get("usuarios", [PaginaController::class, "verUsuarios"]);
