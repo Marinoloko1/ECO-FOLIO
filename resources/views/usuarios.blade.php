72102e08c6b39be5edf6bb823f66449fee68d46e
Angel Escamilla <elprro0019th@gmail.com>
2024-05-17 20:15:53 -0600

    cambios de css

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -139,8 +139,8 @@
         }
 
         agregarUsuario("123", "Maria Fernanda", "1 de 3", "1234567890");
-        agregarUsuario("123", "Maria Fernanda", "1 de 3", "1234567890");
-        agregarUsuario("123", "Maria Fernanda", "1 de 3", "1234567890");
+        agregarUsuario("123", "Maria Fernanda", "2 de 3", "1234567890");
+        agregarUsuario("123", "Maria Fernanda", "3 de 3", "1234567890");
     </script>
 </body>
 </html>
