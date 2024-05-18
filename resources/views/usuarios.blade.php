46af5e33f71502ff85289761a667932d4f64806c
Angel Escamilla <elprro0019th@gmail.com>
2024-05-17 20:50:44 -0600

    cambios de css

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -10,10 +10,9 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background: #52c234;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #061700, #52c234);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #061700, #52c234); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+            background: #1D976C;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #93F9B9, #1D976C);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #93F9B9, #1D976C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
             margin: 0;
             padding: 0;
@@ -22,9 +21,9 @@
         .container {
             max-width: 800px;
             margin: 50px auto;
-            background: #52c234;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #061700, #52c234);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #061700, #52c234); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            background: #1D976C;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #93F9B9, #1D976C);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #93F9B9, #1D976C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
             padding: 20px;
             border-radius: 8px;
