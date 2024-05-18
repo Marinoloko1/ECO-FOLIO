774cbd14e3c04ed7147180b198e96cff0c8eced1
Angel Escamilla <elprro0019th@gmail.com>
2024-05-17 20:30:40 -0600

    cambios de css

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -10,9 +10,10 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background: #1D976C;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #93F9B9, #1D976C);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #93F9B9, #1D976C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            background: #52c234;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #061700, #52c234);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #061700, #52c234); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 
             margin: 0;
             padding: 0;
@@ -27,7 +28,7 @@
 
             padding: 20px;
             border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 0 10px rgba(255, 255, 255, 10);
         }
 
         h1 {
@@ -73,6 +74,10 @@
 
         input[type="submit"]:hover {
             background-color: #45a049; 
+        }
+        #agregarUsuario
+        {
+            color: white;
         }
     </style>
 </head>
